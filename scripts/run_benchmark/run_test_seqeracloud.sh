664016f6c0738d00e1e5dafcb56180801db3d49c
Robrecht Cannoodt <rcannood@gmail.com>
2024-09-07 18:12:38 +0200

    update scripts

--- scripts/run_benchmark/run_test_seqeracloud.sh
+++ scripts/run_benchmark/run_test_seqeracloud.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# get the root of the directory
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# ensure that the command below is run from the root of the repository
+cd "$REPO_ROOT"
+
+set -e
+
+# write the parameters to file
 cat > /tmp/params.yaml << 'HERE'
 input_states: s3://openproblems-data/resources_test/task_label_projection/**/state.yaml
 rename_keys: 'input_train:output_train;input_test:output_test;input_solution:output_solution'
@@ -16,4 +25,4 @@ tw launch https://github.com/openproblems-bio/task_label_projection.git \
   --params-file /tmp/params.yaml \
   --entry-name auto \
   --config common/nextflow_helpers/labels_tw.config \
-  --labels label_projection,test
+  --labels task_label_projection,test
